ceacfdc909c93aa93ba5c0cef20893b5d18902fe
KimJonghwan <army@ncc.re.kr>
2025-06-11 10:30:02 +0900

    Update train_options.py

--- options/train_options.py
+++ options/train_options.py
@@ -35,6 +35,9 @@ class TrainOptions(BaseOptions):
         parser.add_argument('--pool_size', type=int, default=50, help='the size of image buffer that stores previously generated images')
         parser.add_argument('--lr_policy', type=str, default='linear', help='learning rate policy. [linear | step | plateau | cosine]')
         parser.add_argument('--lr_decay_iters', type=int, default=50, help='multiply by a gamma every lr_decay_iters iterations')
-
+        # k-fold cross validation parameters
+        parser.add_argument('--k_folds', type=int, default=5, help='number of folds for k-fold cross validation')
+        parser.add_argument('--val_ratio', type=float, default=0.2, help='ratio of validation set size to training set size')
+        
         self.isTrain = True
         return parser
